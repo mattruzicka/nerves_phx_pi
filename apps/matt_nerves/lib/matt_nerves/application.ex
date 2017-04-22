79b755e8e9f28e9146b02027de539b6ccdf8ca4d
Matt Ruzicka <mattruzicka@gmail.com>
2017-04-22 15:29:55 -0500

    Add a worker that sets up networking

--- apps/matt_nerves/lib/matt_nerves/application.ex
+++ apps/matt_nerves/lib/matt_nerves/application.ex
@@ -9,6 +9,7 @@ defmodule MattNerves.Application do
     # Define workers and child supervisors to be supervised
     children = [
       # worker(MattNerves.Worker, [arg1, arg2, arg3]),
+      worker(Task, [fn -> Nerves.Networking.setup :eth0, [mode: "dhcp"] end], restart: :transient)
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
