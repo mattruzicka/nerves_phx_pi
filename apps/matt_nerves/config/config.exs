0af30d099d7097e4d8b5f8ddddc5ea4cd19e4146
Matt Ruzicka <mattruzicka@gmail.com>
2017-04-22 15:30:14 -0500

    Configure endpoint

--- apps/matt_nerves/config/config.exs
+++ apps/matt_nerves/config/config.exs
@@ -10,3 +10,15 @@ use Mix.Config
 # Uncomment to use target specific configurations
 
 # import_config "#{Mix.Project.config[:target]}.exs"
+
+# Configures the endpoint
+config :matt_phx, MattPhx.Web.Endpoint,
+  http: [port: 80],
+  url: [host: "localhost", port: 80],
+  secret_key_base: "HBI3ket+RsU+bkoUfS/frXAbVsf6giR2lOrkfB3rxMvlfRblB8p9Zjx+SEZgw5aA",
+  root: Path.dirname(__DIR__),
+  server: true,
+  render_errors: [accepts: ~w(html json)],
+  pubsub: [name: Nerves.PubSub]
+
+config :logger, level: :info
